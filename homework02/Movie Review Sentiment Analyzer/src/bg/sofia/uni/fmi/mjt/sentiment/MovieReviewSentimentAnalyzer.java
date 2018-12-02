36e723fb6214b33e84d4019c933fc0823c75d5b8
Kristiyan Gergov <kristiqn1gergov@gmail.com>
2018-12-02 20:55:22 +0200

    Started homework 2

---
+++ homework02/Movie Review Sentiment Analyzer/src/bg/sofia/uni/fmi/mjt/sentiment/MovieReviewSentimentAnalyzer.java
@@ -0,0 +1,11 @@
+package bg.sofia.uni.fmi.mjt.sentiment;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class MovieReviewSentimentAnalyzer {
+
+    public MovieReviewSentimentAnalyzer(InputStream stopwordsInput, InputStream reviewsInput, OutputStream reviewsOutput){
+
+    }
+}
