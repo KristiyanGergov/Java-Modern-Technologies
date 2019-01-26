b2d19492a2e74747000792ca48c733372f520fe7
Kristiyan Gergov <kristiqn1gergov@gmail.com>
2019-01-26 12:36:10 +0200

    Initial test-driven development

---
+++ Final-Project/src/bg/sofia/uni/fmi/mjt/battleships/server/GameServer.java
@@ -0,0 +1,63 @@
+package bg.sofia.uni.fmi.mjt.battleships.server;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class GameServer {
+
+    @Test
+    public void testInitializationOfFieldIsCorrect() {
+        fail();
+    }
+
+    @Test
+    public void testAddShip() {
+        fail();
+    }
+
+    @Test
+    public void testShotChangesField() {
+        fail();
+    }
+
+    @Test
+    public void testShotReturnsCorrectResult() {
+        fail();
+    }
+
+    @Test
+    public void testGetAllActiveGames() {
+        fail();
+    }
+
+    @Test
+    public void testJoinSpecificGame() {
+        fail();
+    }
+
+    @Test
+    public void testJoinRandomGame() {
+        fail();
+    }
+
+    @Test
+    public void testPrintAllSavedGames() {
+        fail();
+    }
+
+    @Test
+    public void testSaveGame() {
+        fail();
+    }
+
+    @Test
+    public void testLoadGame() {
+        fail();
+    }
+
+    @Test
+    public void testDeleteGame() {
+        fail();
+    }
+}
